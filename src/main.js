183cafab6842b9dd58d111537861f0ba7ec7f82a
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-04-03 02:22:40 +0200

    1.3. Add rest of markup functions and render page

--- src/main.js
+++ src/main.js
@@ -31,15 +31,91 @@ const getSortMarkup = () => {
   );
 };
 
+const getFilmsMainMarkup = () => {
+  return (
+    `<section class="films"></section>`
+  );
+};
 
-const render = (parent, markup, place) => {
+const getFilmsListMarkup = () => {
+  return (
+    `<section class="films-list">
+      <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+      <div class="films-list__container">
+      </div>
+    </section>`
+  );
+};
+
+const getFilmsListExtraMarkup = (title) => {
+  return (
+    `<section class="films-list--extra">
+      <h2 class="films-list__title">${title}</h2>
+    
+      <div class="films-list__container">
+      </div>
+    </section>`
+  );
+};
+
+const getLoadMoreButtonMarkup = () => {
+  return (
+    `<button class="films-list__show-more">Show more</button>`
+  );
+};
+
+const getFilmCardMarkup = () => {
+  return (
+    `<article class="film-card">
+      <h3 class="film-card__title">The Dance of Life</h3>
+      <p class="film-card__rating">8.3</p>
+      <p class="film-card__info">
+        <span class="film-card__year">1929</span>
+        <span class="film-card__duration">1h 55m</span>
+        <span class="film-card__genre">Musical</span>
+      </p>
+      <img src="./images/posters/the-dance-of-life.jpg" alt="" class="film-card__poster">
+      <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
+      <a class="film-card__comments">5 comments</a>
+      <form class="film-card__controls">
+        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
+        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
+        <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
+      </form>
+    </article>`
+  );
+};
+
+
+const render = (parent, markup, place = `beforeend`) => {
   parent.insertAdjacentHTML(place, markup);
 };
 
 
 const header = document.querySelector(`.header`);
-render(header, getProfileMarkup(), `beforeend`);
+render(header, getProfileMarkup());
 
 const main = document.querySelector(`.main`);
-render(main, getMainNavMarkup(), `beforeend`);
-render(main, getSortMarkup(), `beforeend`);
+render(main, getMainNavMarkup());
+render(main, getSortMarkup());
+render(main, getFilmsMainMarkup());
+
+const filmsMain = document.querySelector(`.films`);
+render(filmsMain, getFilmsListMarkup());
+render(filmsMain, getFilmsListExtraMarkup(`Top rated`));
+render(filmsMain, getFilmsListExtraMarkup(`Most commented`));
+
+const filmsList = document.querySelector(`.films-list`);
+render(filmsList, getLoadMoreButtonMarkup());
+
+const filmsListContainer = document.querySelector(`.films-list__container`);
+for (let i = 0; i < 5; i++) {
+  render(filmsListContainer, getFilmCardMarkup());
+}
+
+// Obviously temporary solution
+const filmsExtraListsContainers = document.querySelectorAll(`.films-list--extra .films-list__container`);
+for (let container of filmsExtraListsContainers) {
+  render(container, getFilmCardMarkup());
+  render(container, getFilmCardMarkup());
+}
